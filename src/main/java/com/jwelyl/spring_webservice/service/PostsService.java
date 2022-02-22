d9fd5a8ea1a2b388b7c087c30abb865c6d823bc6
허재성 <heojaeseong@heojaeseong-ui-MacBookPro.local>
2022-02-22 18:42:07 +0900

    수정 기능 구현(테스트 포함)

--- src/main/java/com/jwelyl/spring_webservice/service/PostsService.java
+++ src/main/java/com/jwelyl/spring_webservice/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.jwelyl.spring_webservice.service;
 
+import com.jwelyl.spring_webservice.domain.posts.Posts;
 import com.jwelyl.spring_webservice.domain.posts.PostsRepository;
+import com.jwelyl.spring_webservice.web.dto.PostsResponseDto;
 import com.jwelyl.spring_webservice.web.dto.PostsSavedRequestDto;
+import com.jwelyl.spring_webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSavedRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
