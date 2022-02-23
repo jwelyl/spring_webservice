9996829b975a77660b88db3a74916c2a8919caab
허재성 <heojaeseong@heojaeseong-ui-MacBookPro.local>
2022-02-23 15:34:30 +0900

    Annotation based Login

--- src/main/java/com/jwelyl/spring_webservice/web/IndexController.java
+++ src/main/java/com/jwelyl/spring_webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jwelyl.spring_webservice.web;
 
+import com.jwelyl.spring_webservice.config.auth.LoginUser;
 import com.jwelyl.spring_webservice.config.auth.dto.SessionUser;
 import com.jwelyl.spring_webservice.service.PostsService;
 import com.jwelyl.spring_webservice.web.dto.PostsResponseDto;
@@ -16,16 +17,15 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
+
         return "index";
     }
 
