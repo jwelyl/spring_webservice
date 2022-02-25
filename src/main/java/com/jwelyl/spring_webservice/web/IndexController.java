6bec0bf18a6082e285bb24679b8a3abcc45172fa
허재성 <heojaeseong@heojaeseong-ui-MacBookPro.local>
2022-02-25 21:50:04 +0900

    Google Login, USER 권한 부여 후 재 로그인 정상 확인

--- src/main/java/com/jwelyl/spring_webservice/web/IndexController.java
+++ src/main/java/com/jwelyl/spring_webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jwelyl.spring_webservice.web;
 
+import com.jwelyl.spring_webservice.config.auth.dto.SessionUser;
 import com.jwelyl.spring_webservice.service.PostsService;
 import com.jwelyl.spring_webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
